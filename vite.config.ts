23aa28a3f5c1c5b730fb6ac80e66eef6fd090e4e
youssef lamouchi <yousseflamouchi100@gmail.com>
2024-12-10 19:38:25 +0100

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // Remplacez "Portfolio" par le nom exact de votre dépôt GitHub
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // Si vous avez des dépendances à exclure
   },
 });
